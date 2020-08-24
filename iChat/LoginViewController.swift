02aaad2ff5274ecf2c1b2d28819f84b0e5fc5389
nikita-kravtsov <payneblr@gmail.com>
2020-08-24 18:12:49 +0300

    Created new third screen LoginViewController: Added new file: LoginViewController and created new class LoginViewController Realized setupConstraints in extension

---
+++ iChat/LoginViewController.swift
@@ -0,0 +1,99 @@
+//
+//  LoginViewController.swift
+//  iChat
+//
+//  Created by Никита on 8/24/20.
+//  Copyright © 2020 Никита. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupConstraints()
+    }
+    
+    let welcomeBackLabel = UILabel(text: "Welcom back!", font: .avenir26())
+    
+    let loginWith = UILabel(text: "Login with")
+    let orLabel = UILabel(text: "or")
+    let emailLabel = UILabel(text: "Email")
+    let passwordLabel = UILabel(text: "Password")
+    let needAnAccountLabel = UILabel(text: "Need an account?")
+    
+    let emailTextField = OneLineTextField(font: .avenir20())
+    let passwordTextField = OneLineTextField(font: .avenir20())
+    
+    let googleButton = UIButton(title: "Google", titleColor: .black, backgroundColor: .white, isShadow: true)
+    let loginButton = UIButton(title: "Login", titleColor: .white, backgroundColor: .buttonDark())
+    let signUpButton = UIButton(title: "Sign Up", titleColor: .red, backgroundColor: .white)
+    
+}
+
+extension LoginViewController {
+    private func setupConstraints() {
+        
+        let loginWithView = ButtomView(label: loginWith, buttom: googleButton)
+        let emailStackView = UIStackView(arrangedSubviews: [emailLabel, emailTextField], axis: .vertical, spacing: 0)
+        let passwordStackView = UIStackView(arrangedSubviews: [passwordLabel, passwordTextField], axis: .vertical, spacing: 0)
+        
+        loginButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [loginWithView,
+                                                       orLabel,
+                                                       emailStackView,
+                                                       passwordStackView,
+                                                       loginButton], axis: .vertical, spacing: 40)
+        
+        let signUpStackView = UIStackView(arrangedSubviews: [needAnAccountLabel, signUpButton], axis: .horizontal, spacing: -1)
+        
+        view.addSubview(welcomeBackLabel)
+        view.addSubview(stackView)
+        view.addSubview(signUpStackView)
+        
+        welcomeBackLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        signUpStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            welcomeBackLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            welcomeBackLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: welcomeBackLabel.bottomAnchor, constant: 100),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+        signUpStackView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 60),
+        signUpStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+        signUpStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+}
+
+//MARK: - SwiftUI Canvas
+import SwiftUI
+struct LoginVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let loginVC = LoginViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<LoginVCProvider.ContainerView>) -> LoginViewController {
+            return loginVC
+        }
+        
+        func updateUIViewController(_ uiViewController: LoginVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<LoginVCProvider.ContainerView>) {
+            
+        }
+    }
+}
